980b0cde136675527b0cc7dab6a5c34c64543f81
Kevin Poteau <kevin.poteau.ext@gifi.fr>
2024-08-08 17:22:37 +0200

    Ajout des heures et minutes

--- demoDate.py
+++ demoDate.py
@@ -4,7 +4,7 @@ from datetime import datetime
 currentTime = datetime.now()
 
 # Formater la date au format "DD-MM-YYYY"
-date_formatee = currentTime.strftime("%d-%m-%Y")
+date_formatee = currentTime.strftime("%d-%m-%Y %Hh%mm")
 
 # Afficher la date formatée
 print(f"La date d'aujourd'hui est : {date_formatee}")
